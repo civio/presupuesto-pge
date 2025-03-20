534ef2884067f3a8cd874b4e2ed2de6bb9fedf05
David Cabo <david.cabo@gmail.com>
2025-03-20 22:22:22 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,1 +1,6 @@
-from pge_budget_loader import PGEBudgetLoader
+import six
+
+if six.PY2:
+    from pge_budget_loader import PGEBudgetLoader
+else:
+    from .pge_budget_loader import PGEBudgetLoader
--- views/__init__.py
+++ views/__init__.py
@@ -1,2 +1,8 @@
-from ccaa import ccaa
-from articles import *
+import six
+
+if six.PY2:
+    from ccaa import ccaa
+    from articles import *
+else:
+    from .ccaa import ccaa
+    from .articles import *
