00028c3243aca759b2a1ec863481a0af7eb7e6f8
David Cabo <david.cabo@gmail.com>
2025-12-05 16:00:16 +0100

    Add CCAA budgets for 2025 civio/presupuesto-management#1389

--- static/javascripts/ccaa.js
+++ static/javascripts/ccaa.js
@@ -6,7 +6,7 @@ function RegionComparisonMap(budget_data_URL, population_data_URL, geo_data_URL)
         "Canarias","Cantabria","Castilla y León","Castilla La Mancha","Cataluña","Comunitat Valenciana",
         "Extremadura","Galicia","Madrid","Murcia","Navarra","País Vasco","La Rioja","Ceuta","Melilla"];
 
-  var availableYears = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+  var availableYears = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
       balanceByYearAndByRegion,
       balanceByYearByType,
       balanceByYearByRegionByType,
